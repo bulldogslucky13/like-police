e3667f6c49514c005ad335e96187c5357c8f6d6f
Cameron Bristol <cameron.bristol@handsdowndigital.com>
2021-08-21 22:17:42 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello from your Express.JS Server!</h1>");
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Run that server!"));
