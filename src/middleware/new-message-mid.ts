fc715eff035a23ed4c68545b0b427ae47eda9c82
Cameron Bristol <cameron.bristol@handsdowndigital.com>
2021-08-22 14:07:34 -0500

    What does user addition and removal look like...

--- src/middleware/new-message-mid.ts
+++ src/middleware/new-message-mid.ts
@@ -7,6 +7,8 @@ export const newMessageMid = async (
   next: express.NextFunction
 ) => {
   try {
+    // tslint:disable-next-line:no-console
+    console.log(req.body);
     await GroupMeNewTextSchema.validateAsync(req.body);
   } catch (error) {
     res.status(500).json({ message: error.message });
