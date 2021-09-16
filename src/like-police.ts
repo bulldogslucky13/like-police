cd4689ba5e1ee4fbb613e445543a4b3149cf90f2
Cameron Bristol <cameron.bristol@handsdowndigital.com>
2021-09-16 09:48:03 -0500

    Init

--- src/like-police.ts
+++ src/like-police.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import dotenv from "dotenv";
 import moment from "moment";
-import schedule from "node-schedule";
 import { sendingJobs } from "./jobs/jobs";
 
 if (process.env.NODE_ENV !== "production") {
@@ -41,18 +40,23 @@ class LikePolice {
   };
 
   isApprovedSender = async (senderId: string) => {
-    if (this.approvedSenders.length < 1) {
-      await axios.post(`https://api.groupme.com/v3/bots/post`, {
-        bot_id: process.env.BOT_ID,
-        text: "LikePolice is not configured!",
-      });
-      return false;
-    }
-    if (!this.approvedSenders.includes(senderId)) {
-      await axios.post(`https://api.groupme.com/v3/bots/post`, {
-        bot_id: process.env.BOT_ID,
-        text: "Leave me alone, peasant.",
-      });
+    try {
+      if (this.approvedSenders.length < 1) {
+        await axios.post(`https://api.groupme.com/v3/bots/post`, {
+          bot_id: process.env.BOT_ID,
+          text: "LikePolice is not configured!",
+        });
+        return false;
+      }
+      if (!this.approvedSenders.includes(senderId)) {
+        await axios.post(`https://api.groupme.com/v3/bots/post`, {
+          bot_id: process.env.BOT_ID,
+          text: "Leave me alone, peasant.",
+        });
+        return false;
+      }
+    } catch (error) {
+      console.log(error.message);
       return false;
     }
     return true;
